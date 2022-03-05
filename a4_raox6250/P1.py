e489b073ae753681886901df94b43afcb81c8f63
Nausher Rao <SherRao15@gmail.com>
2022-03-04 20:45:23 -0500

    Fix small issues

--- a4_raox6250/P1.py
+++ a4_raox6250/P1.py
@@ -6,25 +6,25 @@ def main():
         raise Exception("Program needs a file to run!");
         exit();
 
-    numbers, pluses, minuses = processFile(str(args[0]));
+    numbers, pluses, minuses = processFile(str(args[1]));
     maxNumbers = pluses + minuses;
     numbers.sort();
 
     result = "";
     sum = 0;
     i = len(numbers) - 1;
-    for n in positives:
+    for n in range(pluses):
         if(i == len(numbers)):
             break;
 
-        result += numbers[i];
+        result += str(numbers[i]);
         if(i >= 1):
             result += " + ";
 
         sum += numbers[i];
         i -= 1;
 
-    for n in negatives:
+    for n in range(minuses):
         if(i == len(numbers)):
             break;
 
@@ -35,19 +35,23 @@ def main():
         sum -= numbers[i];
         i -= 1;
 
-    result += " = " + sum;
+    result += " = " + str(sum);
     return result;
 
 
 def processFile(fileName):
     file = open(fileName, "r+");
     lines = file.readlines();
+    print(lines);
     if(len(lines) < 3):
         raise Exception("File must have atleast three lines!");
         exit();
 
-    numbers = lines[0].split(",");
-    positives = int(lines[1]);
-    negatives = int(lines[2]);
+    numbers = lines[0].strip().split(",");
+    positives = int(lines[1].strip());
+    negatives = int(lines[2].strip());
 
     return [int(x) for x in numbers], positives, negatives;
+
+if(__name__ == "__main__"):
+    print(main());
